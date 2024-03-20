ed5ccd26dfdb41345c83d67988699449c9ac700e
stefegg <stephenegbert@gmail.com>
2024-03-20 11:53:51 -0400

    setup searchbar and plug in to product list, cleanup prisma client invocations

--- src/app/categories/[id]/page.tsx
+++ src/app/categories/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "../../../../lib/prisma";
 import ProductList from "@/app/components/productList";
 
 async function getCatProducts(id: string) {
   "use server";
-  const prisma = new PrismaClient();
   const res = await prisma.product.findMany({
     where: {
       categoryId: id,
@@ -15,12 +14,24 @@ async function getCatProducts(id: string) {
   return res;
 }
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams?: { query?: string };
+}) {
   const { id } = params;
+  const query = searchParams?.query || "";
   const products = await getCatProducts(id);
+  const filteredProducts = Array.isArray(products)
+    ? products.filter((product) => {
+        return product.name.toLowerCase().includes(query.toLowerCase());
+      })
+    : [];
   return (
     <>
-      <ProductList catId={id} products={products} />
+      <ProductList catId={id} products={filteredProducts} />
     </>
   );
 }
