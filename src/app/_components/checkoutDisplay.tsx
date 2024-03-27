f377f81b962130f3dce74f78193f0ac0f784f602
stefegg <stephenegbert@gmail.com>
2024-03-27 12:31:41 -0400

    more styling

--- src/app/_components/checkoutDisplay.tsx
+++ src/app/_components/checkoutDisplay.tsx
@@ -5,8 +5,15 @@ import { Button } from ".";
 import { lobsterFont } from "../fonts";
 
 export default function CheckoutDisplay() {
-  const { cart, setCart, setCartQuantity } = useContext(CartContext);
+  const { cart, setCart, cartQuantity, setCartQuantity } =
+    useContext(CartContext);
   const { appTheme } = useContext(ThemeContext);
+
+  const getSubtotal = () => {
+    let subtotal = 0;
+    cart.map((c) => (subtotal = c.price * c.quantity + subtotal));
+    return subtotal;
+  };
 
   return (
     <div
@@ -20,10 +27,27 @@ export default function CheckoutDisplay() {
           className={`flex border-b-2 border-${appTheme}-text h-20 rounded-tr-lg items-center text-4xl bg-${appTheme}-containerBg`}
         >
           <div className={`pl-4 ${lobsterFont.className} rounded-tr-lg`}>
-            Order Summary
+            Your Order
           </div>
         </div>
-        <div>Order Details</div>
+        <div>
+          <div className={`pl-4 text-2xl pt-2 mb-6`}>
+            {`Order Details (${cartQuantity} item${
+              cartQuantity > 1 ? "s" : null
+            })`}
+          </div>
+          <div className="gap-6 flex flex-col">
+            {cart.map((c, idx) => (
+              <div
+                className={`px-6 text-xl flex flex-row justify-between w-full`}
+                key={idx}
+              >
+                <div>{c.name}</div>
+                <div>x {c.quantity}</div>
+              </div>
+            ))}
+          </div>
+        </div>
         <div>Order Total</div>
       </div>
     </div>
