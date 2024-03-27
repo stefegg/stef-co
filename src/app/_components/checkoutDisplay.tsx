ef9235071b2274c86b5b28e1240b3d46207f4025
stefegg <stephenegbert@gmail.com>
2024-03-27 16:48:00 -0400

    more checkout styling

--- src/app/_components/checkoutDisplay.tsx
+++ src/app/_components/checkoutDisplay.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { CartContext, ThemeContext } from "../_providers";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Button } from ".";
 import { lobsterFont } from "../fonts";
+import { currencyGen } from "../_utils";
 
 export default function CheckoutDisplay() {
   const { cart, setCart, cartQuantity, setCartQuantity } =
     useContext(CartContext);
   const { appTheme } = useContext(ThemeContext);
-
+  const [shippingCost, setShippingCost] = useState(10);
+  const [subTotal, setsubTotal] = useState(0);
   const getSubtotal = () => {
     let subtotal = 0;
     cart.map((c) => (subtotal = c.price * c.quantity + subtotal));
@@ -16,40 +18,90 @@ export default function CheckoutDisplay() {
   };
 
   return (
-    <div
-      className={`flex flex-row h-full border-${appTheme}-text border-2 rounded-lg`}
-    >
-      <div className="w-3/4 bg-blue-500 rounded-l-lg">left</div>
-      <div
-        className={`w-1/4 h-full bg-${appTheme}-bodyBgflex rounded-r-lg border-l-2 border-${appTheme}-text`}
-      >
+    <>
+      {cart.length > 0 ? (
         <div
-          className={`flex border-b-2 border-${appTheme}-text h-20 rounded-tr-lg items-center text-4xl bg-${appTheme}-containerBg`}
+          className={`flex flex-row border-${appTheme}-text border-2 rounded-lg h-full`}
         >
-          <div className={`pl-4 ${lobsterFont.className} rounded-tr-lg`}>
-            Your Order
-          </div>
-        </div>
-        <div>
-          <div className={`pl-4 text-2xl pt-2 mb-6`}>
-            {`Order Details (${cartQuantity} item${
-              cartQuantity > 1 ? "s" : null
-            })`}
-          </div>
-          <div className="gap-6 flex flex-col">
-            {cart.map((c, idx) => (
-              <div
-                className={`px-6 text-xl flex flex-row justify-between w-full`}
-                key={idx}
-              >
-                <div>{c.name}</div>
-                <div>x {c.quantity}</div>
+          <div className="w-3/4 bg-blue-500 rounded-l-lg">left</div>
+          <div
+            className={`w-1/4  bg-${appTheme}-bodyBg h-full flex flex-col justify-between rounded-r-lg border-l-2 border-${appTheme}-text`}
+          >
+            <div
+              className={`flex border-b-2 border-${appTheme}-text rounded-tr-lg h-[10%] items-center text-4xl bg-${appTheme}-containerBg`}
+            >
+              <div className={`pl-6 ${lobsterFont.className} rounded-tr-lg  `}>
+                Your Order
               </div>
-            ))}
+            </div>
+            <div className="h-[60%]">
+              <div
+                className={`pl-6 text-lg h-[10%] flex items-center border-b-2 border-${appTheme}-text`}
+              >
+                {`Order Details (${cartQuantity} item${
+                  cartQuantity > 1 ? "s" : ""
+                })`}
+              </div>
+              <div className="flex flex-col overflow-y-scroll h-[90%]">
+                {cart.map((c, idx) => (
+                  <div
+                    className={`px-6 text-base flex min-h-[10%] flex-row justify-between items-center w-full`}
+                    key={idx}
+                  >
+                    <div>{c.name}</div>
+                    <div className=" min-w-[24px]">x {c.quantity}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div
+              className={`flex flex-col h-[30%] border-t-2 border-${appTheme}-text bg-${appTheme}-containerBg w-full gap-2 rounded-br-lg justify-center`}
+            >
+              <div className="flex flex-row pl-6 text-lg justify-between pr-4">
+                <div>Subtotal:</div>
+                <div>
+                  {currencyGen(cart[0].currency)}
+                  {getSubtotal()}
+                </div>
+              </div>
+              <div className="flex flex-row pl-6 text-lg justify-between pr-4">
+                <div>Shipping & Handling:</div>
+                <div>
+                  {currencyGen(cart[0].currency)}
+                  {shippingCost}
+                </div>
+              </div>
+              <div className="flex flex-row pl-6 text-lg justify-between pr-4">
+                <div>Tax:</div>
+                <div>
+                  {currencyGen(cart[0].currency)}
+                  {shippingCost}
+                </div>
+              </div>
+              <div
+                className={`${lobsterFont.className} text-2xl pl-6  w-full flex flex-row justify-between pr-4`}
+              >
+                <div>Order Total:</div>
+                <div>
+                  {currencyGen(cart[0].currency)}
+
+                  {shippingCost}
+                </div>
+              </div>
+              <div className="flex flex-row pl-6 text-xl justify-between pr-4">
+                <Button
+                  buttonText="Submit Order"
+                  size="lg"
+                  styleType="secondary"
+                  onClick={() => console.log("click")}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div>Order Total</div>
-      </div>
-    </div>
+      ) : (
+        "Cart Empty"
+      )}
+    </>
   );
 }
--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -3,12 +3,7 @@ import { PageWrapper, ListHeader, CheckoutDisplay } from "../_components";
 export default function Checkout() {
   return (
     <PageWrapper>
-      <div className={`flex flex-col h-full pb-6`}>
-        <div className="mb-2">
-          <ListHeader title={"Checkout"} />
-        </div>
-        <CheckoutDisplay />
-      </div>
+      <CheckoutDisplay />
     </PageWrapper>
   );
 }
