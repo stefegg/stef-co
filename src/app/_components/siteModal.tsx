617cf498cce2613040b2e0402dee9af02b145a8a
stefegg <stephenegbert@gmail.com>
2024-06-05 14:27:45 -0400

    opacity updates for containerBg class users

--- src/app/_components/siteModal.tsx
+++ src/app/_components/siteModal.tsx
@@ -58,7 +58,7 @@ export default function SiteModal(props: SiteModalProps) {
   return (
     showModal && (
       <div
-        className={`absolute w-full h-full bg-${appTheme}-containerBg/50 top-0  overflow-hidden cursor-pointer`}
+        className={`absolute w-full h-full bg-${appTheme}-containerBg/50 top-0  overflow-hidden cursor-pointer bg-opacity-80`}
         onClick={() => setShowModal(false)}
       >
         <div className="flex items-center justify-center h-full">
