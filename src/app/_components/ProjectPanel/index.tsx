4417461bc7894b33addd91baca2b28a85e213213
stefegg <stephenegbert@gmail.com>
2024-06-10 12:00:14 -0400

    display stack on smaller screenss

--- src/app/_components/ProjectPanel/index.tsx
+++ src/app/_components/ProjectPanel/index.tsx
@@ -22,14 +22,16 @@ const ProjectPanel = (props: ProjectPanelProps) => {
       <div
         className={`h-24 bg-${appTheme}-containerBg rounded-t-md text-${appTheme}-secondary flex flex-row items-center justify-center py-4 gap-8`}
       >
-        <div className={`text-xl md:text-5xl flex items-end md:items-center`}>
+        <div
+          className={`text-xl md:text-5xl flex items-end md:items-center font-extralight`}
+        >
           {title}
         </div>
         <div
           className={`hidden md:flex w-4 h-4 bg-${appTheme}-text rounded-full `}
         />
         <div
-          className={`text-xl md:text-4xl text-${appTheme}-link flex h-full items-center md:items-end py-2`}
+          className={`text-xl md:text-4xl text-${appTheme}-link flex h-full items-center md:items-end py-2 font-light`}
         >
           <Link href={link}>{linkTitle}</Link>
         </div>
@@ -42,21 +44,28 @@ const ProjectPanel = (props: ProjectPanelProps) => {
       <div
         className={`h-40 bg-${appTheme}-containerBg rounded-b-md py-4 flex flex-row items-center justify-center`}
       >
-        {badgeData.map((data, idx) => {
-          return (
-            <div
-              key={idx}
-              className="h-full w-[12vw] px-4 items-center justify-center hidden md:flex"
-            >
-              <TechBadge
-                title={data.title}
-                src={data.src}
-                textSize={"sm"}
-                bgColor={"bodyBg"}
-              />
-            </div>
-          );
-        })}
+        <div className={`grid grid-cols-3 md:hidden gap-x-4 gap-y-2`}>
+          {badgeData.map((data, idx) => {
+            return (
+              <>
+                <div
+                  key={idx}
+                  className="h-full w-[12vw] px-4 items-center justify-center hidden md:flex"
+                >
+                  <TechBadge
+                    title={data.title}
+                    src={data.src}
+                    textSize={"sm"}
+                    bgColor={"bodyBg"}
+                  />
+                </div>
+                <div className={`text-thin text-${appTheme}-secondary`}>
+                  {data.title}
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
