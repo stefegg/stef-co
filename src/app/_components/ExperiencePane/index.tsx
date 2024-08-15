fbe66f6315a70fef2f2517f053c8e7c8f74e8892
stefegg <stephenegbert@gmail.com>
2024-08-15 15:58:45 -0400

    style experience page

--- src/app/_components/ExperiencePane/index.tsx
+++ src/app/_components/ExperiencePane/index.tsx
@@ -13,14 +13,14 @@ const ExperiencePane = (props: ExperiencePaneProps) => {
     experience: { title, dates, company, location, duties },
   } = props;
   return (
-    <div className="border rounded-lg grid grid-cols-6 p-4">
-      <div className="col-span-6 md:col-span-2 pr-4 pb-4 mr-4 border-b md:border-r md:border-b-0 flex flex-col">
-        <div>{title}</div>
+    <div className="border rounded-lg grid grid-cols-6 p-4 text-lg font-light">
+      <div className="col-span-6 md:col-span-2 pr-4 pb-4 mr-4 border-b md:border-r md:border-b-0 flex flex-col text-link">
+        <div className="text-primary">{title}</div>
         <div>{dates}</div>
         <div>{company}</div>
         <div>{location}</div>
       </div>
-      <div className="col-span-6 md:col-span-4 px-4 mt-4 md:mt-0 flex flex-col">
+      <div className="col-span-6 md:col-span-4 px-4 mt-4 md:mt-0 flex flex-col text-secondary">
         {duties.map((duty, idx) => (
           <li key={idx}>{duty}</li>
         ))}
--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -6,7 +6,10 @@ const Experience = () => {
   return (
     <div className="px-4 md:px-16 pt-12">
       {experiences.map((exp, idx) => (
-        <ExperiencePane key={idx} experience={exp} />
+        <>
+          <ExperiencePane key={idx} experience={exp} />
+          <div className="mt-10" />
+        </>
       ))}
     </div>
   );
