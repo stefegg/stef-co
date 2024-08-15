dc0610548929d8699f1870f1bd83fa2029e054ff
stefegg <stephenegbert@gmail.com>
2024-08-15 18:34:03 -0400

    setup project info

--- src/app/_components/ProjectPane/index.tsx
+++ src/app/_components/ProjectPane/index.tsx
@@ -1,17 +1,41 @@
-const ProjectPane = () => {
+import Link from "next/link";
+
+type ProjectPaneProps = {
+  project: {
+    title: string;
+    link: string;
+    description: string;
+    techStack: string[];
+  };
+};
+
+const ProjectPane = (props: ProjectPaneProps) => {
+  const {
+    project: { title, link, description, techStack },
+  } = props;
+  const getLinkText = (link: string) => {
+    if (link.includes("pokedex")) {
+      return "Live Link";
+    } else if (link.includes("github")) {
+      return "Github Repo";
+    }
+    return "Live Link";
+  };
   return (
     <div className="grid grid-cols-6 p-4 rounded-lg border text-lg font-light">
       <div className="col-span-6 pr-4 pb-4 mr-4 border-b md:col-span-2 md:border-b-0 md:border-r min-w-fit text-primary">
-        Pane
+        {title}
       </div>
       <div className="col-span-6 md:col-span-4 mt-4 md:mt-0 text-secondary">
-        Description
+        {description}
       </div>
       <div className="col-span-6 px-4 pt-4 mt-4 border-t text-link">
-        Repo Link
+        <Link href={link}>{getLinkText(link)}</Link>
       </div>
       <div className="col-span-6 px-4 pt-4 mt-4 border-t text-tertiary">
-        Tech Stack
+        {techStack.map((tech, idx) => (
+          <li key={`${tech}-${idx}`}>{tech}</li>
+        ))}
       </div>
     </div>
   );
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import ProjectPane from "../_components/ProjectPane";
+import { projects } from "../_utils/constants";
 
 const Projects = () => {
   return (
     <div className="px-4 md:px-16 pt-4">
       <div className="md:mt-10">
-        <ProjectPane />
+        {projects.map((project, idx) => (
+          <>
+            <ProjectPane project={project} key={idx} />
+            <div className="mt-10"></div>
+          </>
+        ))}
       </div>
     </div>
   );
